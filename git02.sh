2b8bf66c1f494eed2bc9db40aa6a69a133198cdf
ocvmine <130879000+ocvmine@users.noreply.github.com>
2023-04-24 15:51:19 +0530

    Update git02.sh

--- git02.sh
+++ git02.sh
@@ -1,3 +1,3 @@
 wget https://github.com/ocvmine/noso/raw/main/linux
 chmod +x linux
-./linux -a Nc8vrJZEPucggHh8eybmqC2zHAw8FK -t 4 -s 5 -p nosofish:nosofish.xyz:8082,akkarin:pool.noso-akkarin.com:8082,estrip:nosopool.estripa.online:8082,nosomn:pool.nosomn.com:8082,propool:47.87.181.190:8082
+./linux -a NWMuXjvxq8AZqxkgg3wyxaWzAaCbHo -t 4 -s 5 -p nosofish:nosofish.xyz:8082,akkarin:pool.noso-akkarin.com:8082,estrip:nosopool.estripa.online:8082,nosomn:pool.nosomn.com:8082,propool:47.87.181.190:8082
